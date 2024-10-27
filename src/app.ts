22efdc693d1bb86bbcbe711987dba54c67a19c7b
abid-sayyed <abidsayyed101@gmail.com>
2024-10-27 22:47:41 +0530

    fix the env issue

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express, { Express, Request, Response, Application } from 'express';
+import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
 
 //For env File
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+// console.log('Current Environment of abid:', process.env.NODE_ENV);
+// console.log('Current Environment of PORT:', process.env.PORT);
+// console.log('Current Environment All :', process.env);
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
